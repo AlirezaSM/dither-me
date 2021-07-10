c3287a0d7d9c9891bc0ad8d62a3357a301be36c5
AlirezaSM <sedighi_a77@yahoo.com>
2021-07-10 17:57:27 +0430

    8-bit grayscale implemented.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello project!")
+from PIL import Image
+
+in_img = Image.open("input.jpg")
+width, height = in_img.size
+
+pixels = in_img.load()
+
+for i in range(width):
+    for j in range(height):
+        c_list = list(pixels[i, j])
+        y = int(0.2126 * c_list[0] + 0.7152 * c_list[1] + 0.0722 * c_list[2])
+        pixels[i, j] = (y, y, y)
+
+in_img.show()
+in_img.save('output.jpg')
